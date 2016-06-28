568a08e1f1872378a477131fc0d99e730525eaf0
Ven Korolev <mohtaiiihuk@gmail.com>
2016-06-29 02:38:56 +0700

    wip

--- app/actions.js
+++ app/actions.js
@@ -1,29 +1,45 @@
 import {singIn, singUp} from './utils/api';
 import {browserHistory} from 'react-router';
 
-const authorize = () => {
-  // FB.api('/me?fields=name,email', (response) => {
-    singIn({
-      email: 'www@ww.ru',//response.email,
-      password: '123'//response.id,
-    }).then(data => {
-      console.log(data);
+const authorize = async (dispatch) => {
+  FB.api('/me?fields=name,email', async (response) => {
+    try {
+      const user = await singIn({
+        email: response.email,
+        password: response.id,
+      });
+
+      if (!user.rows.length) {
+        const newUser = await singUp({
+          first_name: response.name,
+          last_name: response.name,
+          email: response.email,
+          password: response.id,
+        });
+      }
+      console.log(dispatch)
       dispatch({
         type: 'LOGIN',
         payload: response,
       });
-      // browserHistory.push('/main');
-    })
-  // });
+
+      browserHistory.push('/main')
+    } catch (e) {
+      dispatch({
+        type: 'LOGIN_ERROR',
+        payload: JSON.stringify(e),
+      });
+    }
+  });
 }
 
 export const login = () => {
   return (dispatch) => {
     FB.login((response) => {
-      authorize();
+      authorize(dispatch);
       return;
       if (response.status === 'connected') {
-        authorize();
+        authorize(dispatch);
       } else if (response.status === 'not_authorized') {
         dispatch({
           type: 'LOGIN_ERROR',
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,17 +1,25 @@
 const API = 'http://recruitment.safetydata.us:3000/v1';
 
+const toQuery = (obj) => {
+  return Object.keys(obj).map((key) => `${key}=${obj[key]}`).join('&');
+}
+
 export const singIn = (data) => {
-  console.log(data);
   return fetch(`${API}/users/whoami`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: JSON.stringify(data),
+    body: toQuery(data),
   }).then(res => res.json());
 }
 
 export const singUp = (data) => {
-  return fetch(`${API}/users/create`, {method: 'POST', body: new FormData(data)})
-    .then(res => res.json());
+  return fetch(`${API}/users/create`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: toQuery(data),
+  }).then(res => res.json());
 }
